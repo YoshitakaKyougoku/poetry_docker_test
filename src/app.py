053a11ee123c8c64beaeac534b8c719ae6c88bab
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2023-08-19 00:32:03 +0000

    不要なコードの削除

--- src/app.py
+++ src/app.py
@@ -1,15 +1,12 @@
 import streamlit as st
-from random import randint
 import requests
 import json
 
 st.title('APIテスト画面（ユーザー）')
 
 with st.form(key='user'):
-    # user_id: int = randint(0, 10)
     user_name: str = st.text_input('ユーザー名',max_chars=12)
     data = {
-        # 'user_id': user_id,
         'user_name':user_name,
     }
     submit_button = st.form_submit_button(label='送信')
